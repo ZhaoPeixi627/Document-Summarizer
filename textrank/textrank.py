4f954d953fce5bc4f00bb33ce2d207283c9ef16f
ZhaoPeixi627 <54923817+ZhaoPeixi627@users.noreply.github.com>
2020-04-27 20:01:10 -0400

    Update textrank.py

--- textrank/textrank.py
+++ textrank/textrank.py
@@ -7,6 +7,8 @@ from model import build_similarity_matrix
 from model import get_topk_keywords
 from model import get_topk_sentences
 from model import pagerank
+import os
+import sys
 
 def extract_keywords(sentences, k=5):
     filtered_sentences = filter_sentences(sentences, lowercase=False, stem=False)
@@ -28,17 +30,23 @@ def summarize(sentences, k=5):
 
     return get_topk_sentences(ranks, sentences, k)
 
-def main():
+def gettxt(filename):
+    # os.makedirs(filename)
     parser = argparse.ArgumentParser()
     parser.add_argument("-p", "--path", help="Path to text file")
     parser.add_argument("-s", "--summarize", action="store_true",
         help="Summarize text, otherwise extract keywords")
     parser.add_argument("-l", "--len", type=int,
         help="Number of keywords/sentences to extract")
+    parser.add_argument("-t", "--output", help="Path to output text file")
+ 
 
     args = parser.parse_args()
 
     k = 5
+    
+    x =filename+'.txt'
+    f = open(x,'w+')
     if args.len:
         k = args.len
 
@@ -46,10 +54,15 @@ def main():
     
     if args.summarize:
         summary = summarize(sentences, k)
-        print(" ".join(summary))
+        # print(" ".join(summary))
+        print(" ".join(summary),file = f)
         return
-    
-    print("; ".join(extract_keywords(sentences, k)))
+        # print("; ".join(extract_keywords(sentences, k)))
+    print("; ".join(extract_keywords(sentences, k)), file = f)
+    f.close()
 
 if __name__ == '__main__':
-    main()
+
+    filename = sys.argv[-1]
+    gettxt(filename)
+   
